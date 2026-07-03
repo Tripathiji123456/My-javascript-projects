c498604385fdfeac7c44f27e052eff7a138b0eaa
Tripathiji123456 <jitripathi804@gmail.com>
2026-07-03 23:50:30 +0530

    Create project6.js

---
+++ project6.js
@@ -0,0 +1,32 @@
+let customerName = "Tripathi Tejaswita";
+
+let laptop = 55000;
+let mouse = 800;
+let keyboard = 1500;
+
+let total = laptop + mouse + keyboard;
+
+let discount = 5000;
+
+let finalBill = total - discount;
+
+console.log("==========================================");
+console.log("         SHOPPING BILL");
+console.log("==========================================");
+
+console.log("Customer Name :", customerName);
+
+console.log("------------------------------------------");
+
+console.log("Laptop        :", laptop);
+console.log("Mouse         :", mouse);
+console.log("Keyboard      :", keyboard);
+
+console.log("------------------------------------------");
+
+console.log("Total Amount  :", total);
+console.log("Discount      :", discount);
+console.log("Final Bill    :", finalBill);
+
+console.log("==========================================");
+

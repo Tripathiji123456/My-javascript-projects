ed9d6f2c3a93233ba03ec7838c6485570c24e5b4
Tripathiji123456 <jitripathi804@gmail.com>
2026-07-03 23:35:00 +0530

    Create project1.js

---
+++ project1.js
@@ -0,0 +1,36 @@
+let studentName = "Tejaswita Tripathi";
+let rollNumber = 102;
+let age = 20;
+let gender = "Female";
+let college = "Parul University";
+let course = "B.Tech";
+let branch = "Computer Science";
+let semester = 5;
+let cgpa = 8.9;
+let city = "Vadodara";
+let state = "Gujarat";
+let mobile = "9876543210";
+let email = "tejaswita@gmail.com";
+
+
+console.log("==========================================");
+console.log("         STUDENT INFORMATION CARD");
+console.log("==========================================");
+
+console.log("Student Name :", studentName);
+console.log("Roll Number  :", rollNumber);
+console.log("Age          :", age);
+console.log("Gender       :", gender);
+console.log("College      :", college);
+console.log("Course       :", course);
+console.log("Branch       :", branch);
+console.log("Semester     :", semester);
+console.log("CGPA         :", cgpa);
+console.log("City         :", city);
+console.log("State        :", state);
+console.log("Mobile       :", mobile);
+console.log("Email        :", email);
+
+console.log("==========================================");
+
+

b31f8a262de3a99d6a749fa5fc90c09bf7ab6317
Tripathiji123456 <jitripathi804@gmail.com>
2026-07-03 23:49:49 +0530

    Create project5.js

---
+++ project5.js
@@ -0,0 +1,25 @@
+let radius = 10;
+
+let pi = 3.14;
+
+
+let diameter = 2 * radius;
+
+let circumference = 2 * pi * radius;
+
+let area = pi * radius * radius;
+
+
+
+console.log("==========================================");
+console.log("         CIRCLE CALCULATOR");
+console.log("==========================================");
+
+console.log("Radius        :", radius);
+console.log("Diameter      :", diameter);
+console.log("Circumference :", circumference);
+console.log("Area          :", area);
+
+console.log("==========================================");
+
+

a8775d035da499ffa12c8692adf4e916d143100d
Tripathiji123456 <jitripathi804@gmail.com>
2026-07-03 23:46:15 +0530

    Create project2.js

---
+++ project2.js
@@ -0,0 +1,20 @@
+let num1 = 25;
+let num2 = 10;
+
+console.log("======================");
+console.log("  SIMPLE CALCULATOR  ");
+console.log("======================");
+
+console.log("First Number  :", num1);
+console.log("Second Number :", num2);
+
+console.log("=======================");
+
+console.log("Addition       :", num1 + num2);
+console.log("Subtraction    :", num1 - num2);
+console.log("Multiplication :", num1 * num2);
+console.log("Division       :", num1 / num2);
+console.log("Modulus        :", num1 % num2);
+console.log("Power          :", num1**2);
+
+console.log("=====================");

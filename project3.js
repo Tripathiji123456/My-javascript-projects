0e386edcf21873de3ced0a574753c8a8714bb7e1
Tripathiji123456 <jitripathi804@gmail.com>
2026-07-03 23:47:41 +0530

    Create project3.js

---
+++ project3.js
@@ -0,0 +1,25 @@
+let name = "Tripathi Tejaswita";
+let currentAge = 19;
+
+let nextYear = currentAge + 1;
+let after5Years = currentAge + 5;
+let after10Years = currentAge + 10;
+let after20Years = currentAge + 20;
+let before5Years = currentAge - 5;
+
+
+console.log("=====================");
+console.log("AGE CALCULATOR");
+console.log("=====================");
+
+console.log("Name                :", name);
+console.log("Current Age         :", currentAge);
+
+console.log("Next Year Age       :", nextYear);
+console.log("Age After 5 Years   :", after5Years);
+console.log("Age After 10 Years  :", after10Years);
+console.log("Age After 20 Years  :", after20Years);
+console.log("Age Before 5 Years  :", before5Years);
+
+console.log("==========================================");
+
